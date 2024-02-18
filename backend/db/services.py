9aa14b56797d83a3d9a39f995c5863a30219fd18
pypivo <artempshulika@mail.ru>
2024-02-18 14:46:21 +0300

    debug sql lock

--- backend/db/services.py
+++ backend/db/services.py
@@ -9,9 +9,9 @@ from .models import Messages
 
 
 async def max_counter_lock(session, message):
-    statement = select(Messages).where(
-        (Messages.counter == select(func.max(Messages.counter))) & (Messages.name == message.name) )
-    rs = await session.execute(statement)
+    subquery = select(func.max(Messages.counter)).where(Messages.name == message.name).scalar_subquery()
+    query = select(Messages).where(Messages.counter == subquery, Messages.name == message.name)
+    rs = await session.execute(query)
     message_with_max_counter: Messages = rs.scalar()
     if message_with_max_counter is not None:
         await session.execute(text(f'SELECT pg_advisory_xact_lock({message_with_max_counter.id})'))
@@ -21,8 +21,8 @@ async def add_message(session: AsyncSession, message):
     async with session.begin() as conn:
 
         await max_counter_lock(session=conn.session, message=message)
-        statement = select(func.max(Messages.counter)).where(Messages.name == message.name)
-        rs = await conn.session.execute(statement)
+        query = select(func.max(Messages.counter)).where(Messages.name == message.name)
+        rs = await conn.session.execute(query)
         max_counter = rs.scalar()
         if max_counter is None:
             max_counter = 0
